9e551c072d9c06ced5c97fda287f58302f5d6163
Andreia Vieira <andreiavieira2353@gmail.com>
2022-11-02 21:57:13 -0300

    first commit

--- index.js
+++ index.js
@@ -1,5 +1,34 @@
-import express from "express"
+import express from "express" //OK
+import cors from "cors" //Ok
 
-const app = express();
 
-app.listen(5000)
+const app = express(); //OK
+app.use(cors());//OK
+app.get("/tweets", (req, res) => {
+const tweets = 
+        [{
+            username: "bobesponja",
+                avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
+              tweet: "eu amo o hub"
+        },
+            {
+                username: "bobesponja",
+                    avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
+                  tweet: "eu amo o hub"
+            },
+            {
+                username: "bobesponja",
+                    avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
+                  tweet: "eu amo o hub"
+            }
+        ]
+    
+
+
+    res.send(tweets);
+});
+
+app.listen(5000,()=>{
+    
+    console.log("Rodando")
+})
