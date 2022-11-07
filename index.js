5014b80769c9e8ec0766df91f4545ff24eae7617
Andreia Vieira <andreiavieira2353@gmail.com>
2022-11-06 21:45:08 -0300

    tweets com foto

--- index.js
+++ index.js
@@ -1,34 +1,53 @@
-import express from "express" //OK
-import cors from "cors" //Ok
+import express from "express"
+import cors from "cors"
+
+const tweets = []
+const users = []
+let avatarUser = ""
+let bodyTweet = {}
+const app = express();
+app.use(cors());
+app.use(express.json())
 
 
-const app = express(); //OK
-app.use(cors());//OK
+app.post("/sign-up", (req, res) => {
+
+    const { username, avatar } = req.body
+    const user = {
+        username,
+        avatar
+    };
+    avatarUser = user.avatar
+    users.push(user)
+
+
+    res.send(user, () => "OK")
+});
+
+
+
+
+app.post("/tweets", (req, res) => {
+    const { username, tweet } = req.body
+    const newTweet = {
+        username,
+        tweet
+    };
+    { tweets.length > 10 & tweets.splice(9, 1) }
+    bodyTweet = { username: newTweet.username, avatar: avatarUser, tweet: newTweet.tweet }
+    tweets.unshift(bodyTweet)
+
+    res.send(tweets, () => "OK")
+})
+
 app.get("/tweets", (req, res) => {
-const tweets = 
-        [{
-            username: "bobesponja",
-                avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
-              tweet: "eu amo o hub"
-        },
-            {
-                username: "bobesponja",
-                    avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
-                  tweet: "eu amo o hub"
-            },
-            {
-                username: "bobesponja",
-                    avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
-                  tweet: "eu amo o hub"
-            }
-        ]
-    
-
 
     res.send(tweets);
 });
 
-app.listen(5000,()=>{
-    
-    console.log("Rodando")
+
+
+app.listen(5000, () => {
+
+
 })
