f634bf2908fbe0e24d3e96769051690af48cfd4f
Andreia Vieira <andreiavieira2353@gmail.com>
2022-11-07 15:17:42 -0300

    Data validation

--- index.js
+++ index.js
@@ -1,53 +1,69 @@
 import express from "express"
 import cors from "cors"
 
-const tweets = []
-const users = []
-let avatarUser = ""
-let bodyTweet = {}
+const tweets = [];
+const users = [];
+let avatarUser = "";
+let bodyTweet = {};
 const app = express();
+
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
 
 
 app.post("/sign-up", (req, res) => {
 
-    const { username, avatar } = req.body
+    const { username, avatar } = req.body;
+
     const user = {
         username,
         avatar
     };
+
     avatarUser = user.avatar
-    users.push(user)
 
+    if (!username || !avatar) {
+        res.status(400).send("Todos os campos são obrigatórios");
+        return
+    } else {
+        users.push(user)
+        res.send("OK")
+    }
 
-    res.send(user, () => "OK")
-});
-
-
-
+}
+);
 
 app.post("/tweets", (req, res) => {
-    const { username, tweet } = req.body
+
+    const { username, tweet } = req.body;
+
     const newTweet = {
         username,
         tweet
     };
-    { tweets.length > 10 & tweets.splice(9, 1) }
-    bodyTweet = { username: newTweet.username, avatar: avatarUser, tweet: newTweet.tweet }
-    tweets.unshift(bodyTweet)
 
-    res.send(tweets, () => "OK")
-})
+    { tweets.length > 10 & tweets.splice(9, 1) };
+
+    bodyTweet = { username: newTweet.username, avatar: avatarUser, tweet: newTweet.tweet }
+
+    if (!username || !tweet) {
+        res.status(400).send("Todos os campos são obrigatórios");
+        return
+    } else {
+        tweets.unshift(bodyTweet)
+        res.send("OK")
+    }
+}
+)
 
 app.get("/tweets", (req, res) => {
-
     res.send(tweets);
-});
+}
+);
 
 
 
 app.listen(5000, () => {
-
-
-})
+    console.log("Running in port 5000")
+}
+)
